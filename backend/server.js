a64b3162de078cf117ba1d501ec605d7d0e66197
Benjamin Sam <79420331+bsam04@users.noreply.github.com>
2022-06-23 22:14:59 -0400

    Preparing deploment #2

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
-//Serve frontend
+// Serve frontend
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
